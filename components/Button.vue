d188928e9da23a0c1c9adb42cc29410bf3870949
Niels Dequeker <niels.dequeker@fastmail.com>
2018-10-10 09:28:45 +0200

    Initial commit

---
+++ components/Button.vue
@@ -0,0 +1,17 @@
+<template>
+  <button>Click me</button>
+</template>
+
+<script>
+  export default {
+    name: "Button"
+  }
+</script>
+
+<style scoped>
+
+  button {
+    background: darkolivegreen;
+  }
+
+</style>
